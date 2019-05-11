ee35ba9cf8dc582128afd1244933c812b318fe02
Dmitry Andriyanov <dima117a@yandex-team.ru>
2019-05-11 15:31:34 +0300

    the second command

--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "shri-ext" is now active!');
+		console.error('Congratulations, your extension "shri-ext" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -17,10 +17,20 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		vscode.window.showInformationMessage('Hello SHRI!');
+	});
+
+    context.subscriptions.push(disposable);
+    
+	let disposable2 = vscode.commands.registerCommand('extension.showTime', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showWarningMessage(new Date().toLocaleTimeString());
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable2);
 }
 
 // this method is called when your extension is deactivated
