84a1920bbcad747964d02bc58ad01ab39dca5702
Dmitry Andriyanov <dima117a@yandex-team.ru>
2019-05-29 09:11:45 +0300

    copy.sh

---
+++ copy.sh
@@ -0,0 +1,8 @@
+cp *.json ../entrance-task-3-final/
+cp *.md ../entrance-task-3-final/
+cp .vscodeignore ../entrance-task-3-final/
+cp .gitignore ../entrance-task-3-final/
+cp -r src ../entrance-task-3-final/
+cp -r preview ../entrance-task-3-final/
+cp -r media ../entrance-task-3-final/
+cp -r .vscode ../entrance-task-3-final/
