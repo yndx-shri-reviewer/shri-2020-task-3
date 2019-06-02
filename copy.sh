4c148117c7cf83cbcebf452986c45b5dbc1bef20
Dmitry Andriyanov <dima117a@yandex-team.ru>
2019-06-02 11:37:45 +0300

    правки замечаний от редактора

--- copy.sh
+++ copy.sh
@@ -1,5 +1,6 @@
 cp *.json ../entrance-task-3-final/
 cp *.md ../entrance-task-3-final/
+cp *.png ../entrance-task-3-final/
 cp .vscodeignore ../entrance-task-3-final/
 cp .gitignore ../entrance-task-3-final/
 cp -r src ../entrance-task-3-final/
